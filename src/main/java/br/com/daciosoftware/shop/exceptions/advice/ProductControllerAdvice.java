d7a55d3b7038c574e5f4703951d31a9889e92e1c
Dácio Braga <fdacio@gmail.com>
2024-01-06 03:33:20 -0300

    refatoracao: exceptions

--- src/main/java/br/com/daciosoftware/shop/exceptions/advice/ProductControllerAdvice.java
+++ src/main/java/br/com/daciosoftware/shop/exceptions/advice/ProductControllerAdvice.java
@@ -1,8 +1,12 @@
 package br.com.daciosoftware.shop.exceptions.advice;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,4 +40,23 @@ public class ProductControllerAdvice {
 		error.setDate(LocalDateTime.now());
 		return error;
 	}
+	
+	@ResponseBody
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ErrorDTO handleValidationError(MethodArgumentNotValidException ex) {
+		
+		ErrorDTO error = new ErrorDTO();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		BindingResult result = ex.getBindingResult();
+		List<FieldError> fieldsErrors = result.getFieldErrors();
+		StringBuilder sb = new StringBuilder("Error de Validação: ");
+		for (FieldError fieldError: fieldsErrors) {
+			sb.append(fieldError.getDefaultMessage()+" ");
+		}
+		error.setMessage(sb.toString());
+		error.setDate(LocalDateTime.now());
+		return error;
+	}
+	
 }
