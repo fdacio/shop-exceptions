47ea5e3a8e6e87e75eb7e6568c82fc210ee9e69c
Dácio Braga <fdacio@gmail.com>
2024-08-23 02:45:32 -0300

    refator to docker compose

--- src/main/java/br/com/daciosoftware/shop/exceptions/advice/ProductControllerAdvice.java
+++ src/main/java/br/com/daciosoftware/shop/exceptions/advice/ProductControllerAdvice.java
@@ -1,17 +1,19 @@
 package br.com.daciosoftware.shop.exceptions.advice;
 
-import br.com.daciosoftware.shop.exceptions.dto.ErrorDTO;
-import br.com.daciosoftware.shop.exceptions.exceptions.CategoryNotFoundException;
-import br.com.daciosoftware.shop.exceptions.exceptions.ProductNotFoundException;
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.time.LocalDateTime;
+import br.com.daciosoftware.shop.exceptions.dto.ErrorDTO;
+import br.com.daciosoftware.shop.exceptions.exceptions.CategoryNotFoundException;
+import br.com.daciosoftware.shop.exceptions.exceptions.ProductIdentifieViolationException;
+import br.com.daciosoftware.shop.exceptions.exceptions.ProductNotFoundException;
 
-@ControllerAdvice(basePackages = {"br.com.daciosoftware.shop.product.controller"})
+@ControllerAdvice(basePackages = { "br.com.daciosoftware.shop.product.controller" })
 public class ProductControllerAdvice {
 
     @ResponseBody
@@ -32,6 +34,17 @@ public class ProductControllerAdvice {
         ErrorDTO error = new ErrorDTO();
         error.setStatus(HttpStatus.NOT_FOUND.value());
         error.setMessage("Categoria não encontrada");
+        error.setDate(LocalDateTime.now());
+        return error;
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(ProductIdentifieViolationException.class)
+    public ErrorDTO handleIntegrityViolation(ProductIdentifieViolationException ex) {
+        ErrorDTO error = new ErrorDTO();
+        error.setStatus(HttpStatus.CONFLICT.value());
+        error.setMessage("Identificador do produto já existe");
         error.setDate(LocalDateTime.now());
         return error;
     }
